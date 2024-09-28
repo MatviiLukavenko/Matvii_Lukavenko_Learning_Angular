ab4c55d88a7bdd92d791a3affb6fef9b1ac6259b
Matvii Lukavenko <w0865739@myscc.ca>
2024-09-27 23:48:20 -0400

    made changes acording to pdf, to the best of my understanding of the questions asked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,61 +1,61 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Games } from "./Games";
+import { Games } from './Games';
+import { ContentListComponent } from './content-list/content-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContentListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title:string = 'Matvii-Lukavenko-Learning-Angular';
-  name:string = 'Matvii Lukavenko';
-  favFood:string = 'Pierogi';
-  gamesArray: any;
-}
+  title: string = 'Matvii-Lukavenko-Learning-Angular';
+  name: string = 'Matvii Lukavenko';
+  favFood: string = 'Pierogi';
 
-const gamesArray: Games[] = [
-  {
-    title: 'The Legend of Zelda',
-    yearReleased: 1986,
-    productionCompany: 'Nintendo',
-    genre: 'Action-adventure',
-    isActive: true,
-  },
-  {
-    title: 'Final Fantasy VII',
-    yearReleased: 1997,
-    productionCompany: 'Square Enix',
-    genre: 'RPG',
-  },
-  {
-    title: 'Halo: Combat Evolved',
-    yearReleased: 2001,
-    productionCompany: 'Bungie',
-    genre: 'Shooter',
-    isActive: false,
-  },
-  {
-    title: 'The Witcher 3: Wild Hunt',
-    yearReleased: 2015,
-    productionCompany: 'CD Projekt RED',
-    genre: 'RPG',
-  },
-  {
-    title: 'Minecraft',
-    yearReleased: 2011,
-    productionCompany: 'Mojang',
-    genre: 'Sandbox',
-    isActive: true,
-  },
-  {
-    title: 'Among Us',
-    yearReleased: 2018,
-    productionCompany: 'Innersloth',
-    genre: 'Party',
-    isActive: true,
-  }
-];
+  gamesArray: Games[] = [
+    {
+      title: 'The Legend of Zelda',
+      yearReleased: 1986,
+      productionCompany: 'Nintendo',
+      genre: 'Action-adventure',
+      isActive: true,
+    },
+    {
+      title: 'Final Fantasy VII',
+      yearReleased: 1997,
+      productionCompany: 'Square Enix',
+      genre: 'RPG',
+    },
+    {
+      title: 'Halo: Combat Evolved',
+      yearReleased: 2001,
+      productionCompany: 'Bungie',
+      genre: 'Shooter',
+      isActive: false,
+    },
+    {
+      title: 'The Witcher 3: Wild Hunt',
+      yearReleased: 2015,
+      productionCompany: 'CD Projekt RED',
+      genre: 'RPG',
+    },
+    {
+      title: 'Minecraft',
+      yearReleased: 2011,
+      productionCompany: 'Mojang',
+      genre: 'Sandbox',
+      isActive: true,
+    },
+    {
+      title: 'Among Us',
+      yearReleased: 2018,
+      productionCompany: 'Innersloth',
+      genre: 'Party',
+      isActive: true,
+    }
+  ];
+}
 
